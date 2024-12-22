8ea306259c519387bb4afa7abccce0f046b997a4
Giedrius <kalniusg@gmail.com>
2024-12-22 13:57:31 +0200

    Initial commit

---
+++ AsyncAwaitDemoWPF/DemoMethods.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemoWPF;
+
+public static class DemoMethods
+{
+    public static List<string> PrepData()
+    {
+        List<string> output = new List<string>();
+
+        //resultsWindow.Text = "";
+
+        output.Add("https://www.yahoo.com");
+        output.Add("https://www.google.com");
+        output.Add("https://www.microsoft.com");
+        output.Add("https://www.cnn.com");
+        output.Add("https://www.stackoverflow.com");
+
+        return output;
+    }
+
+    //public static void RunDownloadSync()
+    public static List<WebsiteDataModel> RunDownloadSync()
+    {
+        List<string> websites = PrepData();
+        List<WebsiteDataModel> output = new();
+
+        foreach (string site in websites)
+        {
+            WebsiteDataModel results = DownloadWebsite(site);
+            //ReportWebsiteInfo(results);
+            output.Add(results);
+        }
+
+        return output;
+    }
+
+    public static List<WebsiteDataModel> RunDownloadParallelSync()
+    {
+        List<string> websites = PrepData();
+        List<WebsiteDataModel> output = new();
+
+        // we say run this code inside
+        // you need to say what you are passing in e.g. string
+        // for each item we are dooing Action
+        // Action is a method called in line that do not return value, it just making a call or just do these two things
+        // it is the same as dowload sync, but it does dowload those site in parallel to each other
+        // is does all tasks in same time synchronously and it lock everything up until it is done
+        Parallel.ForEach<string>(websites, (site) =>
+        {
+            WebsiteDataModel results = DownloadWebsite(site);
+            output.Add(results);
+        });
+
+        return output;
+    }
+
+    // when you are returning async you need to always return Task or Task<T>
+    public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsyncV2(IProgress<ProgressReportModel> progress)
+    {
+        List<string> websites = PrepData();
+        List<WebsiteDataModel> output = new();
+        ProgressReportModel report = new();
+
+        await Task.Run(() => {
+            Parallel.ForEach<string>(websites, (site) =>
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                output.Add(results);
+
+                report.SitesDownloaded = output;
+                report.PercentageComplete = (output.Count * 100) / websites.Count;
+                progress.Report(report);
+            });
+        });
+
+        return output;
+    }
+
+    // never return void in async methods,
+    // you can return void if you return for event
+    // convention is that you need to write Async in any method that are gonna use it
+    // so for everybody let know that you use this as async 
+    //private async Task RunDownloadAsync()
+    // IProgress it is generic. everytime when we do progress we can call this and it will bubble up an event to the caller
+    // Recommended to create a model for report with the data you need in it
+    public static async Task<List<WebsiteDataModel>> RunDownloadAsync(IProgress<ProgressReportModel> progress, CancellationToken cancellationToken)
+    {
+        List<string> websites = PrepData();
+        List<WebsiteDataModel> output = new();
+        ProgressReportModel report = new();
+
+        foreach (string site in websites)
+        {
+            // Task.Run let us run it asynchronously, run this code aas our task
+            // we wrapped this code in async bubble
+            // because of this it execute in paralel or first doing first website and saying then to wait for it and blocks it
+            // we can do it in paralel
+            // here we do do this task and wait for it and other do it and wait for it
+            // Task.Run(() wraps and makes the call async when it is not async
+            //WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+            WebsiteDataModel results = await DownloadWebsiteAsync(site);
+            //ReportWebsiteInfo(results);
+            output.Add(results);
+
+            // if CancellationToken is activated we cancel this task and throw exception - operation cancelled exception
+            // it let us e.g. close the connections if we need, not just shut down everything
+            cancellationToken.ThrowIfCancellationRequested();
+
+            report.SitesDownloaded = output;
+            // take total of websites
+            report.PercentageComplete = (output.Count * 100) / websites.Count;
+
+            // this is how you can get progress in Async calls
+            // it gives info for us back
+            // this send back what type you requested
+            progress.Report(report);
+        }
+
+        return output;
+    }
+
+    public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsync()
+    {
+        List<string> websites = PrepData();
+        List<Task<WebsiteDataModel>> tasks = new();
+
+        foreach (string site in websites)
+        {
+            // so this just put then in list all those bubbles and it will run at the same time
+            // tasks.Add(Task.Run(() => DownloadWebsite(site)));
+            tasks.Add(DownloadWebsiteAsync(site));
+        }
+
+        // whenAll does all the tasks we pass in the whole set of them, and you just wait untill all of them is done
+        // this is more advanced, because you do all the list all dowloading and then working with them
+        // so we putting those bubbles in work 
+        // we just get all results and then just loop through them and putting the results
+        // so it will execute much faster because it will first get results
+        // so we just wait for largest to download
+        // waited to all go get and then worked with them
+        // this is not the best mechanism because when it do something it cannot report something back, because it waits for all to be finished
+        // it is good for quite a lot of things but for reporting not
+        var results = await Task.WhenAll(tasks);
+
+        //foreach (var item in results)
+        //{
+        //    ReportWebsiteInfo(item);
+        //}
+
+        return new List<WebsiteDataModel>(results);
+    }
+
+    // Task is to show that we could get the whole bubble
+    // when we return async method but we say keep going we need to have task to get whole bubble and then you can do it later with something
+    private static async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+    {
+        WebsiteDataModel output = new();
+        WebClient client = new();
+
+        output.WebsiteUrl = websiteURL;
+        output.WebsiteData = await client.DownloadStringTaskAsync(websiteURL);
+
+        return output;
+    }
+
+    private static WebsiteDataModel DownloadWebsite(string websiteURL)
+    {
+        WebsiteDataModel output = new();
+        WebClient client = new();
+
+        output.WebsiteUrl = websiteURL;
+        output.WebsiteData = client.DownloadString(websiteURL);
+
+        return output;
+    }
+
+    //private void ReportWebsiteInfo(WebsiteDataModel data)
+    //{
+    //    resultsWindow.Text += $"{data.WebsiteUrl} downloaded: {data.WebsiteData.Length} characters long. {Environment.NewLine}";
+    //}
+}
---
+++ AsyncAwaitDemoWPF/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwaitDemoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // this is for cancellation
+        // controls weteher we cancel or not
+        CancellationTokenSource cts = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // first time it is slow because it needs to find proxy server and second or other time it is faster because it remembers until app closes
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {
+            // this is much more precise that date now
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            // var results = DemoMethods.RunDownloadSync();
+            var results = DemoMethods.RunDownloadParallelSync();
+            PrintResults(results);
+
+            watch.Stop();
+            // this shows elapsed time from previous to this one point
+            // this is really good performace counter for general stuff
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: {elapsedMs}";
+        }
+
+        // you can return void if you return for event
+        private async void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+            // built in in system namespace
+            Progress<ProgressReportModel> progress = new();
+            // event ProgressChanged that we can hook into
+            progress.ProgressChanged += ReportProgress;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            try
+            {
+                // async stopps the timer before it is finishing its work
+                // await RunDownloadAsync();
+                // await RunDownloadParallelAsync();
+                var results = await DemoMethods.RunDownloadAsync(progress, cts.Token);
+                PrintResults(results);
+            }
+            catch (OperationCanceledException)
+            {
+                // you can do not use any exception
+                resultsWindow.Text += $"The async download was cancelled {Environment.NewLine}";
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: {elapsedMs}";
+        }
+
+        // passing as event e
+        private void ReportProgress(object? sender, ProgressReportModel e)
+        {
+            dashboardProgress.Value = e.PercentageComplete;
+            PrintResults(e.SitesDownloaded);
+        }
+
+        private async void parallelExecuteAsync_Click(object sender, RoutedEventArgs e)
+        {
+            Progress<ProgressReportModel> progress = new();
+            progress.ProgressChanged += ReportProgress;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            //var results = await DemoMethods.RunDownloadParallelAsync();
+            var results = await DemoMethods.RunDownloadParallelAsyncV2(progress);
+            PrintResults(results);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: {elapsedMs}";
+        }
+
+        private void cancelOperation_Click(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+        }
+
+        private void PrintResults(List<WebsiteDataModel> results)
+        {
+            resultsWindow.Text = "";
+            foreach (var item in results)
+            {
+                resultsWindow.Text += $"{item.WebsiteUrl} downloaded: {item.WebsiteData.Length} characters long. {Environment.NewLine}";
+            }
+        }
+    }
+}
+
+// sychronous programming is like line you do a,b,c,d work, but if you need a,b,c to work that D should be completed then you use Asynchronous programming.
+// this gives us a lot, because you can wait for some thing to happen and show to user that is loading or something
+// if system will have resources it will try to do all 4 tasks in same time
+
+// when to use await it is when you need to get data at some point for program
+// mostly it is wrapped in async await and Task
+
+
+// so advanced stuff for async is just cancellation of token and getting progress
+
+// on async you can use the app so it is better
