9af316a2f7a86467841cbbddbaf4860ef4fbc26d
Giedrius <kalniusg@gmail.com>
2024-12-22 13:57:31 +0200

    Initial commit

---
+++ AsyncAwaitDemoWPF/WebsiteDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemoWPF
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = String.Empty;
+        public string WebsiteData { get; set; } = String.Empty;
+    }
+}
